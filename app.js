91c692aa71bc7dba63378681b260a624d595ccc4
yamadadesu <kakiharasou+yamada@gmail.com>
2017-03-01 04:22:02 +0900

    hoge

--- app.js
+++ app.js
@@ -4,12 +4,34 @@ var fs = require('fs');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).listen(3000);  // ポート競合の場合は値を変更
+}).listen(3005);  // ポート競合の場合は値を変更
  
 var io = socketio.listen(server);
  
 io.sockets.on('connection', function(socket) {
+    var name;
     socket.on('client_to_server', function(data) {
         io.sockets.emit('server_to_client', {value : data.value});
     });
+
+    socket.on('client_to_server_broadcast', function(data) {
+        socket.broadcast.emit('server_to_client', {value : data.value});
+    });
+
+    socket.on("client_to_server_personal", function(data){
+        var id = socket.id;
+        name = data.value;
+        var personalMessage = "あなたは、" + name + "さんとして入室しました。";
+        io.to(id).emit("server_to_client", {value: personalMessage});
+    });
+
+    socket.on("disconnect", function(){
+        if(!name) {
+            console.log("未入室のまま、どこかへ去っていきました。");
+        } else {
+            var endMessage = name + "さんが退出しました。";
+            io.sockets.emit("server_to_client", {value : endMessage});
+        }
+    });
+
 });
